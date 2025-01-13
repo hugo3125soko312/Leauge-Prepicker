26bd27c9d49b864a378b174b35304ccf87ad6dd8
hugo3125soko312 <valo3125hugo@gmail.com>
2025-01-13 14:16:40 +0100

    layout of CONNECT button
    layout of CONNECT button

--- app/src/main/java/com/example/leaugeprepick/MainActivity.kt
+++ app/src/main/java/com/example/leaugeprepick/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.leaugeprepick.ui.theme.LeaugePrepickTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +27,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun ConnectButton(alingment: String = , ){
+    Box(
+        modifier = Modifier
+            .fillMaxSize() // fill the entire screen
+            .padding(top = 16.dp), // optional padding
+        contentAlignment = Alignment.TopCenter // aligns content to the center of the top
+    ) {
+        Button(onClick = { /* handle button click */ }) {
+            Text("Click Me")
+        }
+    }
+}
+
+@Composable
 fun main2() {
-    // CODE
+    /*  CODE  */
+    ConnectButton()
 }
