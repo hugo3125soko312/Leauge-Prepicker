0b467321e41e1e61e5d0529544b29ac20ce43a50
hugo3125soko312 <valo3125hugo@gmail.com>
2025-01-13 13:32:30 +0100

    removed darkmode bloat
    removed darkmode bloat

--- app/src/main/java/com/example/leaugeprepick/MainActivity.kt
+++ app/src/main/java/com/example/leaugeprepick/MainActivity.kt
@@ -18,30 +18,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LeaugePrepickTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            main2() // init
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LeaugePrepickTheme {
-        Greeting("Android")
-    }
+fun main2() {
+    // CODE
 }
