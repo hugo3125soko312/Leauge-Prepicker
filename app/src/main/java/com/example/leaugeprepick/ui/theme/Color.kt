d66d7f49241798f30d49a3ef12d857ad1e40e671
hugo3125soko312 <valo3125hugo@gmail.com>
2025-01-14 12:55:16 +0100

    Update Color.kt
    updated colors

--- app/src/main/java/com/example/leaugeprepick/ui/theme/Color.kt
+++ app/src/main/java/com/example/leaugeprepick/ui/theme/Color.kt
@@ -10,3 +10,9 @@ val blue = Color(51,115,251)
 val darkWhite = Color(204,221,254)
 val white = Color(254,254,255)
 // RATING
+val ratingSp = Color(247,142,49) // Sp
+val ratingS = Color(67,110,229) // S
+val ratingA = Color(114,147,220) // A
+val ratingB = Color(221,244,255) // B
+val ratingC = Color(240,178,184) // C
+val ratingD = Color(177,54,74) // D
